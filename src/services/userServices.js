8564510c4b7ec4e94413b2b189f44c8accebbb1a
minsung <menysung94@naver.com>
2024-06-27 14:55:09 +0900

    로그인 서비스 추가

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -11,3 +11,8 @@ export async function signup(user, profile) {
   // axios로 서버에 유저폼데이터 post 요청한다
   await apiClient.post("/user/signup", body);
 }
+
+// 로그인 함수 : 유저객체를 입력한다
+export async function login(user) {
+  await apiClient.post("/user/login", user);
+}
