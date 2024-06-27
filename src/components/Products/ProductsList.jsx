185ed268eaf6d8d83ab00c9cf0448df89fd67fbe
minsung <menysung94@naver.com>
2024-06-27 12:24:40 +0900

    상품 증가 감소 기능 구현

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import useData from "../../Hook/useData";
+import Pagination from "../Common/Pagination";
 import ProductCard from "./ProductCard";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 import "./ProductsList.css";
@@ -7,14 +8,21 @@ import "./ProductsList.css";
 const ProductsList = () => {
   const [search, setSearch] = useSearchParams(); // ?(물음표) 뒤의 쿼리스트링 가져온다
   const category = search.get("category"); //쿼리스트링에서 category=값을 가져온다
-  console.log("카테고리 :" + category);
+  const page = search.get("page");
+  //console.log("카테고리 :" + category);
   //서버에서 가져오는 데이터에는 제품데이터 및 페이지등 다른 데이터들 있음.
   const { data, error, isLoading } = useData(
     "products",
-    { params: { category } },
-    [category]
+    { params: { category, page } },
+    [category, page]
   );
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  //쿼리스트링 search에 페이지를 업데이트한다
+  const handlePageChange = (page) => {
+    const currentParams = Object.fromEntries([...search]);
+    setSearch({ ...currentParams, page: page });
+  };
 
   return (
     <section className="products_list_section">
@@ -46,6 +54,15 @@ const ProductsList = () => {
             />
           ))}
       </div>
+      {/* pagination 넣기 */}
+      {data && (
+        <Pagination
+          total={data.totalProducts} //전체페이지 출력
+          perPage={8} //한 페이지에 8개 출력
+          onClick={handlePageChange} //클릭하면 클릭한 페이지로 쿼리스트링 바뀜
+          currentPage={page}
+        />
+      )}
     </section>
   );
 };
--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -1,63 +1,68 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import useData from "../../Hook/useData";
+import Loader from "../Common/Loader";
 import QuantityInput from "./QuantityInput";
 import "./SingleProductPage.css";
-
-//샘플 제품 객체
-const product = {
-  id: 1,
-  title: "상품 타이틀",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
-  price: 9900,
-  images: [
-    "https://via.placeholder.com/500x500?text=Product+Image+1",
-    "https://via.placeholder.com/500x500?text=Product+Image+2",
-    "https://via.placeholder.com/500x500?text=Product+Image+3",
-    "https://via.placeholder.com/500x500?text=Product+Image+4",
-  ],
-  stock: 10,
-};
 
 const SingleProductPage = () => {
   // 선택한 이미지 기억
   const [selectedImage, setSelectedImage] = useState(0);
+  const { id } = useParams(); //주소 변수 값으로 id 얻기
+  //console.log(id);
+  //id 값으로 제품 데이터 요청한다
+  const { data: product, error, isLoading } = useData(`/products/${id}`);
+  //console.log(product);
+  //구매 증가, 감소 버튼
+  const [quantity, setQuantity] = useState(1); //초기값 1 이다
+
   return (
     <section className="align_center single_product">
-      <div className="align_center">
-        <div className="single_product_thumbnails">
-          {product.images.map((image, index) => (
+      {error && <em className="form_error">{error}</em>}
+      {isLoading && <Loader />}
+      {product._id && (
+        <>
+          <div className="align_center">
+            <div className="single_product_thumbnails">
+              {product.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={`http://localhost:5000/products/${image}`}
+                  alt={product.title}
+                  className={selectedImage === index ? "selected_image" : ""}
+                  onClick={() => setSelectedImage(index)}
+                />
+              ))}
+            </div>
+
+            {/* 큰 이미지는 왼쪽 4개의 이미지중 선택한 인덱스 번호의 이미지 표시 */}
             <img
-              key={index}
-              src={image}
+              src={`http://localhost:5000/products/${product.images[selectedImage]}`}
               alt={product.title}
-              className={selectedImage === index ? "selected_image" : ""}
-              onClick={() => setSelectedImage(index)}
+              className="single_product_display"
             />
-          ))}
-        </div>
+          </div>
 
-        {/* 큰 이미지는 왼쪽 4개의 이미지중 선택한 인덱스 번호의 이미지 표시 */}
-        <img
-          src={product.images[selectedImage]}
-          alt={product.title}
-          className="single_product_display"
-        />
-      </div>
+          <div className="single_product_details">
+            <h1 className="single_product_title">{product.title}</h1>
+            <p className="single_product_description">{product.description}</p>
+            <p className="single_product_price">
+              ￦ {product.price.toLocaleString("ko-KR")} 원
+            </p>
 
-      <div className="single_product_details">
-        <h1 className="single_product_title">{product.title}</h1>
-        <p className="single_product_description">{product.description}</p>
-        <p className="single_product_price">
-          ￦ {product.price.toLocaleString("ko-KR")} 원
-        </p>
+            <h2 className="quantity_title">구매개수:</h2>
+            <div className="align_center quantity_input">
+              <QuantityInput
+                quantity={quantity}
+                setQuantity={setQuantity}
+                stock={product.stock} //재고보다 많은 수량 선택 할 수 없음
+              />
+            </div>
 
-        <h2 className="quantity_title">구매개수:</h2>
-        <div className="align_center quantity_input">
-          <QuantityInput />
-        </div>
-
-        <button className="search_button add_cart">장바구니 추가</button>
-      </div>
+            <button className="search_button add_cart">장바구니 추가</button>
+          </div>
+        </>
+      )}
     </section>
   );
 };
